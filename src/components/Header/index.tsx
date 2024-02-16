8e44c45edccd6f8c8d8306cd53ee9196f7681ed9
holices <dev.holices@gmail.com>
2024-02-16 20:11:26 +0000

    ➕ build: Added Radix UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,31 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import IgniteLogo from '../../assets/ignite-logo.svg'
 
+import * as Dialog from '@radix-ui/react-dialog'
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={IgniteLogo} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>
+                Nova transação
+              </Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
